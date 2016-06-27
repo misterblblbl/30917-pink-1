180ba0e2698a5be66bbafb24741c03000dd74700
Alexandra Godun <mister.blblbl@gmail.com>
2016-06-27 17:41:24 +0300

    Свёрстано всё

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+"use strict";
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync");
+var rename = require("gulp-rename");
+var svgstore = require('gulp-svgstore');
+var svgmin = require('gulp-svgmin');
+
+gulp.task("style", function() {
+  gulp.src("sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({browsers: [
+        "last 1 version",
+        "last 2 Chrome versions",
+        "last 2 Firefox versions",
+        "last 2 Opera versions",
+        "last 2 Edge versions"
+      ]})
+    ]))
+    .pipe(gulp.dest("css"))
+    .pipe(server.reload({stream: true}));
+});
+
+//Создать svg-спрайт из почищенных svg-файлов
+gulp.task('symbols', function() {
+  return gulp.src('img/icons/*.svg')
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('symbols.svg'))
+    .pipe(gulp.dest('img'))
+});
+
+gulp.task("serve", ["style"], function() {
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    ui: false
+  });
+
+  gulp.watch("sass/**/*.{scss,sass}", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+});
